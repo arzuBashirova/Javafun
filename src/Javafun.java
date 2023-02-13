c7f1a1468b84d5bfa29b2de5807672f9d7bfa421
arzuBashirova <124849570+arzuBashirova@users.noreply.github.com>
2023-02-13 14:14:08 -0500

    Update Javafun.java

--- src/Javafun.java
+++ src/Javafun.java
@@ -1,5 +1,6 @@
 public class Javafun {
     public static void main(String[] args) {
+        //I will find the great job so soon!
                     System.out.println(" J      a    v       v   a");
                     System.out.println(" J     a  a    v    v   a  a");
                     System.out.println(" J    aaaaaa     V V   aaaaaa ");
